690f3670d46b33a0167deb1dafb84f883f17a792
Evgeniy Tsarkov <Evgeniy_Tsarkov@epam.com>
2024-10-06 01:03:02 +0300

    DataAccessLayer / Add a Data seeding script to be able to run repository integration tests

---
+++ TicketSpot.Tests/Data seeding.sql
@@ -0,0 +1,14 @@
+﻿USE [TicketSpotDb]
+GO
+SET IDENTITY_INSERT [dbo].[EventManagers] ON 
+GO
+INSERT [dbo].[EventManagers] ([Id], [FirstName], [LastName], [Email]) VALUES (1, N'Elon', N'Musk', N'e.musk@spacex.com')
+GO
+SET IDENTITY_INSERT [dbo].[EventManagers] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Venues] ON 
+GO
+INSERT [dbo].[Venues] ([Id], [Name], [Address], [Description], [EventManagerId]) VALUES (1, N'Dome', N'Las Vegas', N'', 1)
+GO
+SET IDENTITY_INSERT [dbo].[Venues] OFF
+GO
