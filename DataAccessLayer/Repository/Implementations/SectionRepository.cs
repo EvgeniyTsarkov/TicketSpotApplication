1aba0f01e5e00220fc2d4e19b8477eda78662ac9
Evgeniy Tsarkov <Evgeniy_Tsarkov@epam.com>
2024-10-16 02:06:42 +0300

    Data Access Layer / Minor style changes

--- DataAccessLayer/Repository/Implementations/SectionRepository.cs
+++ DataAccessLayer/Repository/Implementations/SectionRepository.cs
@@ -3,49 +3,48 @@ using DataAccessLayer.Exceptions;
 using DataAccessLayer.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
-namespace DataAccessLayer.Repository.Implementations
+namespace DataAccessLayer.Repository.Implementations;
+
+public class SectionRepository(TicketSpotDbContext context) : IRepository<Section>
 {
-    public class SectionRepository(TicketSpotDbContext context) : IRepository<Section>
+    public new async Task<List<Section>> GetAllAsync() =>
+        await context.Sections
+        .AsNoTracking()
+        .AsQueryable()
+        .Include(e => e.Venue)
+        .ToListAsync();
+
+    public async Task<Section> GetAsync(int id) =>
+        await context.Sections
+        .AsNoTracking()
+        .AsQueryable()
+        .Where(e => e.Id == id)
+        .Include(e => e.Venue)
+        .SingleOrDefaultAsync();
+
+    public async Task<Section> CreateAsync(Section sectionToCreate)
     {
-        public new async Task<List<Section>> GetAllAsync() =>
-            await context.Sections
-            .AsNoTracking()
-            .AsQueryable()
-            .Include(e => e.Venue)
-            .ToListAsync();
+        await context.Sections.AddAsync(sectionToCreate);
+        await context.SaveChangesAsync();
+        return sectionToCreate;
+    }
 
-        public async Task<Section> GetAsync(int id) =>
-            await context.Sections
-            .AsNoTracking()
-            .AsQueryable()
-            .Where(e => e.Id == id)
-            .Include(e => e.Venue)
-            .SingleOrDefaultAsync();
+    public async Task<Section> UpdateAsync(Section updatedSection)
+    {
+        var itemToUpdate = await GetAsync(updatedSection.Id)
+            ?? throw new RecordNotFoundException("The section to be updated is not found in the database");
 
-        public async Task<Section> CreateAsync(Section sectionToCreate)
-        {
-            await context.Sections.AddAsync(sectionToCreate);
-            await context.SaveChangesAsync();
-            return sectionToCreate;
-        }
+        context.Sections.Update(updatedSection);
+        await context.SaveChangesAsync();
+        return updatedSection;
+    }
 
-        public async Task<Section> UpdateAsync(Section updatedSection)
-        {
-            var itemToUpdate = await GetAsync(updatedSection.Id)
-                ?? throw new RecordNotFoundException("The section to be updated is not found in the database");
+    public async Task DeleteAsync(int id)
+    {
+        var itemToDelete = new Section { Id = id };
 
-            context.Sections.Update(updatedSection);
-            await context.SaveChangesAsync();
-            return updatedSection;
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var itemToDelete = new Section { Id = id };
-
-            context.Attach(itemToDelete);
-            context.Sections.Remove(itemToDelete);
-            await context.SaveChangesAsync();
-        }
+        context.Attach(itemToDelete);
+        context.Sections.Remove(itemToDelete);
+        await context.SaveChangesAsync();
     }
 }
